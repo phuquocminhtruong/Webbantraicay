12c4dd08f22abf78a96d69197e15bc99426a5bdf
Trường Phù Quốc Minh <minhtruongrox@gmail.com>
2021-01-25 14:54:46 +0700

    first commit

---
+++ webbanqua/bottom.php
@@ -0,0 +1,9 @@
+<table width="960" cellpadding="0" cellspacing="0" border="0" style="border:#CCCCCC 1px solid; margin-top:10px;">
+	<tr>
+		<td height="97" valign="middle" align="center" style="color:white;text-align:center">
+		Copyright © Phù Quốc Minh Trường. <br/>
+		Kỹ Thuật Phần Mềm D17PM03.		
+				
+		</td>		
+	</tr>
+</table>
---
+++ webbanqua/include/dichvu/gioithieu.php
@@ -0,0 +1,48 @@
+<div style="width:587px; margin-left:3px; margin-right:3px;">
+	<table width="587" border="0" cellpadding="0" cellspacing="0">
+		<tr>
+			<td style="height:25px; background:url(images/trang.jpg) repeat-x;" align="center" class="ht" colspan="3">
+				Giới Thiệu
+			</td>
+		</tr>
+		<tr>
+				<td>
+				<table width="587" height="120" cellpadding="0" cellspacing="0" border="0" style="border:#CCCCCC 1px solid; margin-top:3px;">
+					<tr>
+					<td>
+				<p class="pp" align="center">
+					<img src="images/gioithieu.jpg" border="0" width="200" height="166">
+				</p>	
+				<p class="pp">
+				<span class="demuc">Lời Giới Thiệu:</span>
+				<br />
+Các bạn đã không ít lần phải suy nghĩ, băn khoăn khi không biết nên mua các loại trái cây tươi, ngon và là đặc sản của mỗi vùng quê.
+<br />
+ tuy không phải món quà có giá trị cao nhưng qua luôn là món quà thể hiện được sự quan tâm ,tình cảm yêu quý của người tặng với đến với người nhận .
+				</p>
+				<p class="pp">
+trái cây là sự tinh tế trang trọng trong các nhà hàng, khách sạn hay cơ quan công sở, là sự trang trọng lich sự trong các lễ khai trương, các cuộc họp, hội nghị…
+				</p>
+				<p class="pp">
+Xuất phát từ những suy nghĩ đó tôii muốn xây dựng một website chuyên cung cấp các sản phẩm về trái cây phục vụ cho cuộc sông hằng ngày, quà tặng, cung cấp qua định kỳ cho nhà hàng, khách sạn hay cơ quan doanh nghiệp.
+				</p>
+				<p class="pp" align="center">
+					<img src="images/giohang.jpg" width="220" height="62">
+				</p>	
+				<p class="pp">
+ Mong rằng chúng tôi sẽ mang những trái cây ngon nhất, những món quà ý nghĩa nhất đến với người thân yêu của bạn trên khắp mọi miền, khoảng cách về không gian và thời gian không còn là vấn đề nữa các bạn ạ.
+ 				</p>
+				<p class="pp">
+Chúng tôi cũng luôn chú trọng quan tâm đến các văn phòng, công sở, nhà hàng, đặc biệt sẽ có giá ưu đãi cho những cơ quan đặt qua theo định kì dài hạn, hoặc làm qua cho hội nghị, hội thao, những ngày lễ lớn của quý công ty.
+				</p>
+				<p class="pp">
+
+Triết lí kinh doanh của Siêu Thị Trái Cây là mang lại cho quý khách sự hài lòng hơn cả sự mong đợi, bằng chất lượng sản phẩm và dịch vụ tốt.
+</p>
+				</td>
+			</tr>
+		</table>
+				</td>
+		</tr>
+  </table>
+</div>
