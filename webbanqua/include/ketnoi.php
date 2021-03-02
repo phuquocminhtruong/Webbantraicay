3587a5f671bd21e2056456069596313f5f34e9c0
phuquocminhtruong <50606400+phuquocminhtruong@users.noreply.github.com>
2021-03-02 20:05:01 +0700

    Update ketnoi.php

--- webbanqua/include/ketnoi.php
+++ webbanqua/include/ketnoi.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_SERVER','localhost');
+define('DB_SERVER',' 127.0.0.1');
 define('DB_USER','root');
 define('DB_PASS' ,'');
 define('DB_NAME', 'traicay');
-$ung=mysql_connect('localhost','root','') or die('Khong The Ket Noi Voi May Chu');
+$ung=mysql_connect(' 127.0.0.1','root','') or die('Khong The Ket Noi Voi May Chu');
 $strSQL=mysql_select_db('traicay',$ung);
 mysql_query("SET NAMES 'utf8'");
 ?>
